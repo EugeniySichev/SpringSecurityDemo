b7cd2826d6e29c155738cacad7914d4255d12d10
EugeniySichev <sichev91@inbox.ru>
2021-05-20 15:39:17 +0300

    add basic security things

--- src/main/java/com/example/SpringSecurityDemo/model/Developer.java
+++ src/main/java/com/example/SpringSecurityDemo/model/Developer.java
@@ -1,5 +1,19 @@
 package com.example.SpringSecurityDemo.model;
 
-public class Developer {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class Developer {
+    public Developer(long l, String string, String string2) {
+		
+	}
+	private Long id;
+    private String firstName;
+    private String lastName;
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
