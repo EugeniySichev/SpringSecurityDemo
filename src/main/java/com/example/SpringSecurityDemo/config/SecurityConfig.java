36c542a237f473f3b5f1ae208eb6fbe947286c10
EugeniySichev <sichev91@inbox.ru>
2021-05-21 16:40:00 +0300

    add access by permissions

--- src/main/java/com/example/SpringSecurityDemo/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.example.SpringSecurityDemo.model.Permission;
 import com.example.SpringSecurityDemo.model.Role;
 
 @Configuration
@@ -24,9 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/").permitAll()
-			.antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-			.antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
-			.antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+			.antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+			.antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+			.antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Permission.DEVELOPERS_WRITE.getPermission())
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -41,12 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				 User.builder()
 				 	 .username("admin")
 				 	 .password(passwordEncoder().encode("admin"))
-				 	 .roles(Role.ADMIN.name())
+				 	 .authorities(Role.ADMIN.getAuthorities())
 				 	 .build(),
 				 User.builder()
 					 .username("user")
 					 .password(passwordEncoder().encode("user"))
-					 .roles(Role.USER.name())
+					 .authorities(Role.USER.getAuthorities())
 					 .build()
 				 
 				 
